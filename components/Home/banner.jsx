1c79427819d867be865cf9f387034e592e797f05
Samama Islam <Samama.Islam@Biotech.com>
2023-01-24 16:43:02 +0500

    banner changes

--- components/Home/banner.jsx
+++ components/Home/banner.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 function Banner() {
   return (
-      <div  className="bg-[url('/image/landingpage.png')]">
+      <div className="">
+        <img className="w-full" src="/images/landingpage.png" alt="" />
          <div className="container">
-
+s
          </div>
       </div>
   );
